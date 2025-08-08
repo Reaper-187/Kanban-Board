059c569a158c0f669d405ca10f2bf38f1e3dad23
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-08-08 22:05:08 +0200

    Frontend  - setup for Kanban-Board

---
+++ client/src/main.tsx
@@ -0,0 +1,81 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import { Dashboard } from "./Pages/Dashboard.tsx";
+import { Toaster } from "sonner";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/Sidebar/app-sidebar.tsx";
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/login",
+  //   element: (
+  //     <GuestRoute>
+  //       <Login />
+  //     </GuestRoute>
+  //   ),
+  // },
+  // {
+  //   path: "/register",
+  //   element: (
+  //     <GuestRoute>
+  //       <Register />
+  //     </GuestRoute>
+  //   ),
+  // },
+  // {
+  //   path: "/reset-password-authentication",
+  //   element: <ForgotPw />,
+  // },
+  // {
+  //   path: "/verifyUser",
+  //   element: (
+  //     <VerificationRoute>
+  //       <Verification />
+  //     </VerificationRoute>
+  //   ),
+  // },
+  // {
+  //   path: "/One-Time-Otp",
+  //   element: (
+  //     <OtpRoute>
+  //       <OneTimeOtp />
+  //     </OtpRoute>
+  //   ),
+  // },
+  // {
+  //   path: "/change-password",
+  //   element: (
+  //     <OtpRoute>
+  //       <ChangePasswordPage />
+  //     </OtpRoute>
+  //   ),
+  // },
+  {
+    element: <App />,
+    children: [
+      { path: "/", element: <Navigate to="/dashboard" replace /> },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <SidebarProvider>
+      <Toaster />
+      <AppSidebar />
+      <SidebarTrigger />
+      <RouterProvider router={router} />
+    </SidebarProvider>
+  </StrictMode>
+);
