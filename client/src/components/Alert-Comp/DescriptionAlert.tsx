839ca641b23eec3c8a8b58ab6916df2f68a592b7
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-10-01 15:00:00 +0200

    InfoCard HTML Building

--- client/src/components/Alert-Comp/DescriptionAlert.tsx
+++ client/src/components/Alert-Comp/DescriptionAlert.tsx
@@ -1,7 +1,8 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { Card, CardHeader } from "../ui/card";
 import { useToggle } from "@/Context/AddBtnContext";
-import { X } from "lucide-react";
+import { DownloadIcon, X } from "lucide-react";
+import { Button } from "../ui/button";
 
 export const DescriptionAlert = () => {
   const { isDescriptionOpen, closeDescription, currentTask } = useToggle();
@@ -25,7 +26,7 @@ export const DescriptionAlert = () => {
             <Card className="relative p-5">
               <CardHeader className="mb-4 p-0">
                 <div className="flex justify-between items-center">
-                  <h1 className="text-xl font-semibold">Description</h1>
+                  <h1 className="text-xl font-semibold">Ticket-Info</h1>
                   <span
                     onClick={closeDescription}
                     className="cursor-pointer text-gray-500 hover:text-gray-700 transition-colors"
@@ -34,9 +35,31 @@ export const DescriptionAlert = () => {
                   </span>
                 </div>
               </CardHeader>
-              <p className="text-gray-700 flex self-center">
-                {currentTask.description}
-              </p>
+              <div className="space-y-5">
+                <div>
+                  <p className="text-lg font-semibold">Description:</p>
+                  <p className="text-gray-700 flex self-center">
+                    {currentTask.description}
+                  </p>
+                </div>
+                <p className="w-full bg-gray-300 h-[1px]"></p>
+                <div>
+                  <div className="flex justify-between items-center mb-5">
+                    <div className="flex items-center">
+                      <p className="text-lg font-semibold">Attachment</p>
+                      <p className="p-2 rounded-m bg-grey-300">1: file</p>
+                    </div>
+                    <Button className="p-1">+ Add new File</Button>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <p>DOC</p>
+                    <DownloadIcon />
+                  </div>
+                  <div className="mt-5">
+                    <p className="text-lg font-semibold">Activity</p>
+                  </div>
+                </div>
+              </div>
             </Card>
           </motion.div>
         </motion.div>
--- client/src/components/DropDownMenu/DropDown.tsx
+++ client/src/components/DropDownMenu/DropDown.tsx
@@ -3,7 +3,6 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
