000ce15816c1143abba115d4adfaf0a059d8bd4f
Reaper069 <abdulcheik3@gmail.com>
2025-10-02 22:22:13 +0200

    remove useless code

--- client/src/components/Dashboard-Components/Task-Order/Table-view/TableContainer.tsx
+++ client/src/components/Dashboard-Components/Task-Order/Table-view/TableContainer.tsx
@@ -1,31 +1,3 @@
-// import { PlusIcon } from "lucide-react";
-// import { useToggle } from "@/Context/AddBtnContext";
-// import type { HeaderProps } from "../StatusTypes/StatusView";
-
-// export const TableContainer = ({ column, tasks }: HeaderProps) => {
-//   const { openModal } = useToggle();
-
-//   return (
-//     <div className="flex items-center gap-2">
-//       <span className="rounded-full bg-red-200 p-1">
-//         <column.Icon size={13} />
-//       </span>
-//       <h3 className="font-semibold">{column.title}</h3>
-//       <span className="bg-gray-200 rounded-sm px-1 text-sm">
-//         {tasks.length} Tasks
-//       </span>
-//       <button
-//         type="button"
-//         onClick={() => openModal(null)}
-//         className="p-1 rounded-full transition duration-300 cursor-pointer hover:bg-gray-200"
-//         aria-label="Add task"
-//       >
-//         <PlusIcon size={15} />
-//       </button>
-//     </div>
-//   );
-// };
-
 import {
   type ColumnDef,
   type ColumnFiltersState,
