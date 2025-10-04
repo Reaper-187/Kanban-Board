8358512477ea8a4ac8d0a07de23e5f0f7c3063f2
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-10-04 12:21:41 +0200

    feat: global search input to find tasks

--- client/src/components/Dashboard-Components/Task-Order/Table-view/TableContainer.tsx
+++ client/src/components/Dashboard-Components/Task-Order/Table-view/TableContainer.tsx
@@ -143,13 +143,11 @@ export function TableContainer({ tasks }: TableProps) {
     <div className="w-full">
       {/* <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
+  placeholder="Search tasks..."
+  value={globalFilter ?? ""}
+  onChange={(event) => setGlobalFilter(event.target.value)}
+  className="max-w-sm"
+/>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
